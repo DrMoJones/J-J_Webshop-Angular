a1806b27db765d5182f92a3312f93995f31eb831
Jonas Christensen <jonashc@live.dk>
2020-09-25 09:36:03 +0200

    Routing to Shop page

--- J-J-Webshop/src/app/app-routing.module.ts
+++ J-J-Webshop/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TilesComponent } from './tiles/tiles.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Shop', component: TilesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
