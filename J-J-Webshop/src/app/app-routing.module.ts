03ebb620636e69659df8c3f5a858c2ffc956d32f
Jakob Vestergaard <jakobmetalv@gmail.com>
2020-09-28 12:56:54 +0200

    Routing

--- J-J-Webshop/src/app/app-routing.module.ts
+++ J-J-Webshop/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { TilesComponent } from './tiles/tiles.component';
 import { UserComponent } from './user/user.component';
 import { CartComponent } from "./cart/cart.component";
 import { ProductDescriptionComponent } from "./productDescription/productDescription.component";
+import { TestingComponent } from "./testing/testing.component"
 
 const routes: Routes = [
   {path: 'cart', component: CartComponent},
   {path: 'description', component: ProductDescriptionComponent},
   {path: 'shop', component: TilesComponent},
-  {path: 'user', component: UserComponent}
+  {path: 'user', component: UserComponent},
+  {path: 'test', component: TestingComponent}
 ];
 
 @NgModule({
