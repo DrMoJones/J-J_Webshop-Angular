a02cba9a65c622d6b371809a8b10e8bf9d855cde
40162131 <jona190c@elev.tec.dk>
2020-09-29 14:03:14 +0200

    Tiles autogeneration

--- J-J-Webshop/src/app/tiles/tiles.component.ts
+++ J-J-Webshop/src/app/tiles/tiles.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../Services/products.service'
+import { Product } from "../Models/Product";
 
 @Component({
   selector: 'app-tiles',
   templateUrl: './tiles.component.html',
   styleUrls: ['./tiles.component.css']
 })
-export class TilesComponent implements OnInit {
+export class TilesComponent implements OnInit
+{
 
-  constructor() { }
+  products: Product[];
 
-  ngOnInit() {
+  constructor(
+    private productsService: ProductsService
+    ) {}
+
+  ngOnInit()
+  {
+    this.GetProducts();
   }
 
+  GetProducts()
+  {
+    this.productsService.GetProducts()
+    .subscribe(products => this.products = products)
+  }
 }
