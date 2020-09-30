24a0dad888ee00ceb8c8b1cc55ff3b504b0aedbd
Jonas Christensen <jonashc@live.dk>
2020-09-30 11:34:29 +0200

    automated product desc + added design

--- J-J-Webshop/src/app/productDescription/productDescription.component.ts
+++ J-J-Webshop/src/app/productDescription/productDescription.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+
+import { ProductsService } from '../Services/products.service';
+import { Product } from "../Models/Product";
 
 @Component({
   selector: 'app-productDescription',
   templateUrl: './productDescription.component.html',
-  styleUrls: ['./productDescription.component.css']
+  styleUrls: ['./productDescription.component.css'],
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  product: Product;
 
-  ngOnInit() {
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit()
+  {
+    this.GetProduct();
   }
 
+  GetProduct()
+  {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productsService.GetProduct(id)
+    .subscribe(product => this.product = product)
+  }
 
 }
